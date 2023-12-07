d4dd7e2b225469debda4b9ddc30cdce1fcb4fda5
Ronald Arias <ariasron@hotmail.com>
2023-12-07 13:24:33 -0500

    installs css bundle for css bundling third party styles and installs ts-reset to improve types

--- app/root.tsx
+++ app/root.tsx
@@ -7,24 +7,22 @@ import {
 import { type LinksFunction } from "@remix-run/node";
 import faviconAssetUrl from './assets/favicon.svg';
 import fontStylesheetUrl from './styles/font.css';
-import tailwindUrl from './styles/tailwind.css'
+import tailwindUrl from './styles/tailwind.css';
+
+// import css bundles to support importing directly 'some.css' instead of css url
+// NOTE: it's not recommended to bundle all the css files, because if we change one of
+// the css file, it will create a new bundle for all of them. 
+// It's better to separate them and only use bundle for third party styles which we don't modify.
+import { cssBundleHref } from '@remix-run/css-bundle';
+import './styles/global.css';
 
 export const links: LinksFunction = () => {
-  return [
-    {
-      rel: 'icon',
-      type: 'image/svg+xml',
-      href: faviconAssetUrl,
-    },
-    {
-      rel: 'stylesheet',
-      href: fontStylesheetUrl,
-    },
-    {
-      rel: 'stylesheet',
-      href: tailwindUrl,
-    }
-  ]
+	return [
+		{ rel: 'icon', type: 'image/svg+xml', href: faviconAssetUrl },
+		{ rel: 'stylesheet', href: fontStylesheetUrl },
+		{ rel: 'stylesheet', href: tailwindUrl },
+		cssBundleHref ? { rel: 'stylesheet', href: cssBundleHref } : null,
+	].filter(Boolean)
 }
 
 export default function App() {
