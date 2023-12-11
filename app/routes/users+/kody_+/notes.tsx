9046b30b4d0688c1c2577d6fd8e89e207dc8c558
Ronald Arias <ariasron@hotmail.com>
2023-12-11 13:02:34 -0500

    working with links, navbar navigation and adds function to concatenate classnames

--- app/routes/users+/kody_+/notes.tsx
+++ app/routes/users+/kody_+/notes.tsx
@@ -1,9 +1,16 @@
-import { Outlet } from "@remix-run/react"
+import { Link, NavLink, Outlet } from "@remix-run/react";
+import { cn } from "~/utils/misc";
 
 export default function NotesRoute() {
 	return (
 		<div className="flex h-full justify-between pb-12 border-8 border-blue-500">
-			<h1 className="text-h1">Notes</h1>
+			<div>
+				<h1 className="text-h1">Notes</h1>
+				<ul>
+					<li><Link to=".." relative="path" className="underline">Back to Kody</Link></li>
+					<li><NavLink to="some-note-id" className={({isActive}) => cn("underline", isActive && "bg-accent")}>Some Note</NavLink></li>
+				</ul>
+			</div>
       <Outlet />
 		</div>
 	)
