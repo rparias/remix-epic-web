cc1ecfb96beadf8d19de80982f2b9000325f3efd
Ronald Arias <ariasron@hotmail.com>
2024-08-20 16:51:00 -0500

    Adds loader to get data from db

--- app/routes/users+/$username_+/notes.$noteId.tsx
+++ app/routes/users+/$username_+/notes.$noteId.tsx
@@ -1,17 +1,31 @@
-import { useParams } from "@remix-run/react"
+import {useLoaderData, useParams} from "@remix-run/react"
+import { db } from "app/utils/db.server"
+import { LoaderFunctionArgs } from "@remix-run/node";
+
+export async function loader({ request, params }: LoaderFunctionArgs) {
+	const note = db.note.findFirst({
+		where: {
+			id: { equals: params.noteId },
+		},
+	})
+
+	return new Response(JSON.stringify({ note }), {
+		headers: { 'content-type': 'application/json' },
+	})
+}
 
 export default function NoteRoute() {
-	const params = useParams();
-	const { noteId } = params;
+	const data = useLoaderData<typeof loader>();
+	const { note } = data;
 
 	return (
 		<div className="absolute inset-0 flex flex-col px-10">
 			<h2 className="mb-2 pt-12 text-h2 lg:mb-6">
-				{noteId} (🐨 replace this with the title)
+				{ note.title }
 			</h2>
 			<div className="overflow-y-auto pb-24">
 				<p className="whitespace-break-spaces text-sm md:text-lg">
-					🐨 Note content goes here...
+					{ note.content}
 				</p>
 			</div>
 		</div>
