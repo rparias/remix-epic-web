654ea040f1b24be55b4157407cded2fb635d2798
Ronald Arias <ariasron@hotmail.com>
2024-09-06 11:03:49 -0500

    updates styles on edit form

--- app/routes/users+/$username_+/notes.$noteId_.edit.tsx
+++ app/routes/users+/$username_+/notes.$noteId_.edit.tsx
@@ -28,17 +28,24 @@ export default function NoteEdit() {
 	const data = useLoaderData<typeof loader>();
 
 	return (
-		<Form method="post" className="p-8">
-			<div className="mt-4">
-				<Label>Title</Label>
-				<Input name="title" defaultValue={data.note.title} />
-			</div>
-			<div className="mt-4">
-				<Label>Content</Label>
-				<Textarea name="content" defaultValue={data.note.content} />
+		<Form
+			method="post"
+			className="flex h-full flex-col gap-y-4 overflow-x-hidden px-10 pb-28 pt-12"
+		>
+			<div className="flex flex-col gap-1">
+				<div>
+					{/* 🦉 NOTE: this is not an accessible label, we'll get to that in the accessibility exercises */}
+					<Label>Title</Label>
+					<Input name="title" defaultValue={data.note.title} />
+				</div>
+				<div>
+					{/* 🦉 NOTE: this is not an accessible label, we'll get to that in the accessibility exercises */}
+					<Label>Content</Label>
+					<Textarea name="content" defaultValue={data.note.content} />
+				</div>
 			</div>
 			<div className={floatingToolbarClassName}>
-				<Button variant="secondary" type="reset">
+				<Button variant="destructive" type="reset">
 					Reset
 				</Button>
 				<Button type="submit">Submit</Button>
