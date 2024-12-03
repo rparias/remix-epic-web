4d625bf2a33abd1cfd2b9ba7c8f78d490e548b2d
Ronald Arias <ariasron@hotmail.com>
2024-12-03 16:38:50 -0500

    Adds ErrorBoundary in the root to support any error

--- app/root.tsx
+++ app/root.tsx
@@ -12,6 +12,7 @@ import {
 	type MetaFunction,
 } from '@remix-run/react';
 import faviconAssetUrl from './assets/favicon.svg';
+import { GeneralErrorBoundary } from './components/error-boundary.tsx';
 import fontStylesheetUrl from './styles/font.css';
 import tailwindUrl from './styles/tailwind.css';
 import { getEnv } from './utils/env.server.ts';
@@ -35,51 +36,73 @@ export const links: LinksFunction = () => {
 };
 
 export async function loader() {
+	// throw new Error('🐨 root loader error')
 	return json({ username: os.userInfo().username, ENV: getEnv() });
 }
 
-export default function App() {
-	const data = useLoaderData<typeof loader>();
+function Document({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="en" className="h-full overflow-x-hidden">
 			<head>
-				<meta charSet="utf-8" />
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<Meta />
+				<meta charSet="utf-8" />
+				<meta name="viewport" content="width=device-width,initial-scale=1" />
 				<Links />
 			</head>
 			<body className="flex h-full flex-col justify-between bg-background text-foreground">
-				<header className="container mx-auto py-6">
-					<nav className="flex justify-between">
-						<Link to="/">
-							<div className="font-light">epic</div>
-							<div className="font-bold">notes</div>
-						</Link>
-						<Link className="underline" to="users/kody/notes/d27a197e">
-							Kody's Notes
-						</Link>
-					</nav>
-				</header>
-				<div className="flex-1">
-					<Outlet />
-				</div>
-
-				<div className="container mx-auto flex justify-between">
-					<div>
-						<div className="font-light">epic</div>
-						<div className="font-bold">notes</div>
-					</div>
-				</div>
-				<div className="h-5" />
+				{children}
 				<ScrollRestoration />
-				<script
-					dangerouslySetInnerHTML={{
-						__html: `window.ENV = ${JSON.stringify(data.ENV)}`,
-					}}
-				/>
 				<Scripts />
 				<LiveReload />
 			</body>
 		</html>
+	);
+}
+
+export default function App() {
+	// throw new Error('🐨 root component error')
+	const data = useLoaderData<typeof loader>();
+	// 🐨 replace most of this with the <Document> component and render the
+	// header, outlet, and footer inside of it.
+	return (
+		<Document>
+			<header className="container mx-auto py-6">
+				<nav className="flex justify-between">
+					<Link to="/">
+						<div className="font-light">epic</div>
+						<div className="font-bold">notes</div>
+					</Link>
+					<Link className="underline" to="users/kody/notes/d27a197e">
+						Kody's Notes
+					</Link>
+				</nav>
+			</header>
+			<div className="flex-1">
+				<Outlet />
+			</div>
+
+			<div className="container mx-auto flex justify-between">
+				<div>
+					<div className="font-light">epic</div>
+					<div className="font-bold">notes</div>
+				</div>
+			</div>
+			<div className="h-5" />
+			<script
+				dangerouslySetInnerHTML={{
+					__html: `window.ENV = ${JSON.stringify(data.ENV)}`,
+				}}
+			/>
+		</Document>
+	);
+}
+
+export function ErrorBoundary() {
+	return (
+		<Document>
+			<div className="flex-1">
+				<GeneralErrorBoundary />
+			</div>
+		</Document>
 	);
 }
